0a618636c40686103f051cbb6d6e470e2bf0b17b
blackdogeduc-ops <blackdog.educ@gmail.com>
2025-11-30 00:56:51 +0100

    Delete home.js

--- home.js
+++
@@ -1,6 +0,0 @@
-auth.onAuthStateChanged(user=>{
-    if(!user) window.location='index.html';
-    else document.getElementById('username').textContent='Connecté en tant que : '+user.email;
-});
-
-document.getElementById('logout').onclick=()=>auth.signOut();
