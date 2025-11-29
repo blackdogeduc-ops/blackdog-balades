578b767f99753499b272766e224a981ce41031cc
blackdogeduc-ops <blackdog.educ@gmail.com>
2025-11-30 00:55:54 +0100

    Delete profile.js

--- profile.js
+++
@@ -1,5 +0,0 @@
-auth.onAuthStateChanged(user=>{
-    if(!user) window.location='index.html';
-    else document.getElementById('email').textContent=user.email;
-});
-document.getElementById('logout').onclick=()=>auth.signOut();
